e8aa26857b5e67339a7f46ef1c5ba3dec953fbd0
masatoT10 <tsujii26@up.edu>
2023-04-27 19:44:54 -0700

    Updated end screeen

--- app/src/main/java/com/example/othello_superhuman/view/OthelloLocalGame.java
+++ app/src/main/java/com/example/othello_superhuman/view/OthelloLocalGame.java
@@ -50,7 +50,46 @@ public class OthelloLocalGame extends LocalGame {
         OthelloState state = (OthelloState) super.state;
         state.endGame();
         if(state.gameOver){
-            return "Game is Over";
+            if(state.numWhitePieces> state.numBlackPieces){
+                return "White Wins!!\n" + "White pieces: " + state.numWhitePieces + "\n" + "Black pieces: " + state.numBlackPieces + "\n";
+            }
+            if(state.numWhitePieces < state.numBlackPieces) {
+                return "Black Wins!!\n " + "White pieces: " + state.numWhitePieces + "\n" + "Black pieces: " + state.numBlackPieces + "\n";
+
+            }
+            //GameOverMessage
+//            c.drawText("Game Over!", 480, 240, black);
+//            black.setTextSize(75);
+//            if(gameState.isTie){
+//                c.drawText("Tie. No Winner!", 595, 310, black);
+//            }
+//            else{
+//                if(gameState.blackWinner) {
+//                    c.drawText("Black Wins!", 595, 310, black);
+//                }
+//                else {
+//                    c.drawText("White Wins!", 595, 310, black);
+//                }
+//            }
+//            //Score
+//            String blackS = ""+gameState.numBlackPieces;
+//            String whiteS = ""+gameState.numWhitePieces;
+//            c.drawCircle(625, 425 , 100, black);
+//            black.setTextSize(40);
+//            c.drawText(blackS, 613, 560, black);
+//            c.drawCircle(975, 425 , 100, white);
+//            c.drawText(whiteS, 963, 560, black);
+//
+//            //Buttons
+//            c.drawRect(525, 600, 725, 675, brownBox);
+//            c.drawRect(875, 600, 1075, 675, brownBox);
+//            black.setTextSize(50);
+//            c.drawText("Exit", 575, 655, black);
+//            black.setTextSize(50);
+//            c.drawText("Restart", 895, 655, black);
+
+
+//            return "Game is Over";
         }
         return null;
     }
